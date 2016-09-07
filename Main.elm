a4184ad69ea70c9d67eeb7df24efb38ac175693b
Adam Benzan <adam.benzan@gmail.com>
2016-09-07 16:57:24 -0400

    populate URL

--- Main.elm
+++ Main.elm
@@ -1,40 +1,61 @@
 module Portfolio exposing (..)
 
-
 import Debug exposing (log)
+import Array
 
 import Html.App
-
 import Html exposing (..)
-
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, rel, href, src)
 
-type alias Project = { name : String, url : String, active : Bool}
-type alias Model = List Project
-type Msg = SelectProject Project
+type alias Project = { name : String, url : String }
+type alias Model = { projects : List Project, current : Int}
+type Msg = SelectProject Int
+
+
+listGet : List a -> Int -> Maybe a
+listGet xs n = List.head (List.drop n xs)
+
     
 makeProject : String -> String -> Project
 makeProject projectName projectURL =
     { name = projectName
     , url = projectURL
-    , active = False
     }
 
+    
 initialModel : Model
-initialModel = [makeProject "egoop" "clawtros.com"
-               , makeProject "goop2" "clawtros.com"
-               , makeProject "goop3" "clawtros.com"]
+initialModel =
+    { projects = [makeProject "Text Renderer" "http://www.clawtros.com/textrender.html"
+                 , makeProject "goop2" "clawtros.com/2"
+                 , makeProject "goop3" "clawtros.com/3"]
+    , current = 0
+    }
 
+    
+css : String -> Html Msg
+css path =
+  node "link" [ rel "stylesheet", href path ] []
+
+      
 view : Model -> Html Msg
 view model =
-    ul [] (List.map (\e -> li [] [a [onClick (SelectProject e)] [text e.name]]) model)
+    div [] [
+         div [class "menu"] [
+              h1 [ ] [text "test"]
+             , css "http://localhost:8080/elport/styles.css" 
+             , div [] (List.indexedMap (\index -> \project -> div [] [
+                                                   a [onClick (SelectProject index), class (if model.current == index then "active" else "inactive")] [text (project.name ++ " (" ++ project.url ++ ")")]]) model.projects)
+             ]
+        , div [class "content"] [iframe [src (.url (Maybe.withDefault {name="", url=""} (listGet model.projects model.current)))] []]
+        ]
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        SelectProject project ->
-            log project.name
-            model
+        SelectProject index ->
+            {model | current = index}
+                
 
 main : Program Never
 main = Html.App.beginnerProgram
