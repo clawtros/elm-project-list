0bafc4ee268868fc25f6e3bc78f975e3f1092c3d
Adam Benzan <adam.benzan@gmail.com>
2017-01-29 12:47:32 -0500

    update to 0.18

--- ProjectList.elm
+++ ProjectList.elm
@@ -2,11 +2,10 @@ module ProjectList exposing (..)
 
 import Navigation
 import String
-
 import Html exposing (..)
-import Html.App
 import Html.Attributes exposing (class, rel, href, src)
 import Html.Events exposing (onClick)
+import UrlParser exposing ((</>), parseHash)
 
 
 type alias Project =
@@ -24,6 +23,7 @@ type alias Model =
 
 type Msg
     = SelectProject Int
+    | UrlChange Navigation.Location
 
 
 listGet : List a -> Int -> Maybe a
@@ -31,40 +31,32 @@ listGet xs n =
     List.head (List.drop n xs)
 
 
-makeProject : String -> String -> String -> Project
-makeProject projectName projectURL description =
-    { name = projectName
-    , url = projectURL
-    , description = description
-    }
-
-
 initialModel : Model
 initialModel =
     { projects =
-        [ makeProject "Cool Synth" "http://www.clawtros.com/synth/" "WebAudio oscillator toy"
-        , makeProject "Text Renderer" "http://www.clawtros.com/textrender.html" "Drop-in ASCII THREE.js renderer."
-        , makeProject "Goop" "http://clawtros.com/goop/" "I have no idea how this works."
-        , makeProject "Glitchy Art Maker" "http://clawtros.com/backgrounds/" "Attempting to recreate goop."
-        , makeProject "Mandelbrot" "http://mandelbutt.com/" "Raw WebGL Mandelbrot zoomer."
-        , makeProject "Terrain" "http://clawtros.com/maze-terrain/" "Accidentally made while experimenting with maze generation, the A* algorithm and THREE.js"
-        , makeProject "Crossword Player" "http://clawtros.com/clientcross/" "Reacty Crossword player"
-        , makeProject "D3 Update/Exit" "http://clawtros.com/d3.html" "Getting D3 enter/update/exit sorted"
-        , makeProject "Worst Phonetic Dictionary" "http://phonetic.removablefeast.com/" "Bad phonetic dictionaries"
-        , makeProject "Crossword Generator" "http://clawtros.com/recursin-workers/" "Recursive web workers generating crosswords"
-        , makeProject "Floating" "http://clawtros.com/floating.html" "Dots floating on vectors."
-        , makeProject "Marriage" "http://deeznups.clawtros.com/" "Solving life's greatest problems"
-        , makeProject "Lorenz" "http://clawtros.com/waterwheel/" "Playing with Canvas and D3."
-        , makeProject "Voronoi" "http://clawtros.com/voronoi/" "Naive Voronoi shades."
-        , makeProject "Minutes til' Five" "http://minutes-til-five.com/" "Countdown Clock"
-        , makeProject "Ulam Spirals" "http://removablefeast.com/spiral.html" "I think this was the first thing I made with Canvas?"
-        , makeProject "Cat Look" "http://removablefeast.com/catlook" "Cats looking"
-        , makeProject "Deal With Itifier" "http://deal.removablefeast.com/?url=https%3A%2F%2Fcdn-images-1.medium.com%2Fmax%2F1200%2F1*l7zNW_4-afEOfP_mXxs75w.jpeg" "Sunglass Applicator"
-        , makeProject "Drips" "http://clawtros.com/drips" "Averaging HSV colours with surroundings"
-        , makeProject "Bouncing Balls" "http://clawtros.com/google-bouncing-balls/" "Modification of a recreation of a Google Doodle"
-        , makeProject "Alternate Fingering" "http://fingers.removablefeast.com/" ""
-        , makeProject "Name Generators" "http://names.removablefeast.com/" "NLTK"
-        , makeProject "TTC Locator" "http://ttc.removablefeast.com/" "All the Toronto Transit Commission vehicles"
+        [ Project "Cool Synth" "http://www.clawtros.com/synth/" "WebAudio oscillator toy"
+        , Project "Text Renderer" "http://www.clawtros.com/textrender.html" "Drop-in ASCII THREE.js renderer."
+        , Project "Goop" "http://clawtros.com/goop/" "I have no idea how this works."
+        , Project "Glitchy Art Maker" "http://clawtros.com/backgrounds/" "Attempting to recreate goop."
+        , Project "Mandelbrot" "http://mandelbutt.com/" "Raw WebGL Mandelbrot zoomer."
+        , Project "Terrain" "http://clawtros.com/maze-terrain/" "Accidentally made while experimenting with maze generation, the A* algorithm and THREE.js"
+        , Project "Crossword Player" "http://clawtros.com/clientcross/" "Reacty Crossword player"
+        , Project "D3 Update/Exit" "http://clawtros.com/d3.html" "Getting D3 enter/update/exit sorted"
+        , Project "Worst Phonetic Dictionary" "http://phonetic.removablefeast.com/" "Bad phonetic dictionaries"
+        , Project "Crossword Generator" "http://clawtros.com/recursin-workers/" "Recursive web workers generating crosswords"
+        , Project "Floating" "http://clawtros.com/floating.html" "Dots floating on vectors."
+        , Project "Marriage" "http://deeznups.clawtros.com/" "Solving life's greatest problems"
+        , Project "Lorenz" "http://clawtros.com/waterwheel/" "Playing with Canvas and D3."
+        , Project "Voronoi" "http://clawtros.com/voronoi/" "Naive Voronoi shades."
+        , Project "Minutes til' Five" "http://minutes-til-five.com/" "Countdown Clock"
+        , Project "Ulam Spirals" "http://removablefeast.com/spiral.html" "I think this was the first thing I made with Canvas?"
+        , Project "Cat Look" "http://removablefeast.com/catlook" "Cats looking"
+        , Project "Deal With Itifier" "http://deal.removablefeast.com/?url=https%3A%2F%2Fcdn-images-1.medium.com%2Fmax%2F1200%2F1*l7zNW_4-afEOfP_mXxs75w.jpeg" "Sunglass Applicator"
+        , Project "Drips" "http://clawtros.com/drips" "Averaging HSV colours with surroundings"
+        , Project "Bouncing Balls" "http://clawtros.com/google-bouncing-balls/" "Modification of a recreation of a Google Doodle"
+        , Project "Alternate Fingering" "http://fingers.removablefeast.com/" ""
+        , Project "Name Generators" "http://names.removablefeast.com/" "NLTK"
+        , Project "TTC Locator" "http://ttc.removablefeast.com/" "All the Toronto Transit Commission vehicles"
         ]
     , current = 1
     }
@@ -90,7 +82,6 @@ view model =
     div [ class "container" ]
         [ div [ class "menu" ]
             [ h1 [] [ text "clawtros.com" ]
-            , css "styles.css"
             , div [ class "description" ]
                 [ getCurrent model
                     |> .description
@@ -129,58 +120,35 @@ view model =
         ]
 
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    let
-        newModel =
-            case msg of
-                SelectProject index ->
-                    { model | current = index }
-    in
-        ( newModel, Navigation.newUrl (toUrl newModel) )
+    case msg of
+        SelectProject index ->
+            { model | current = index } ! [Navigation.newUrl ("#project/" ++ (toString index))]
+        UrlChange location ->
+            model ! []
 
 
-init : Result String Int -> ( Model, Cmd Msg )
-init result =
-    urlUpdate result initialModel
+parseIdFromLocation : Navigation.Location -> (Maybe Int)
+parseIdFromLocation location =
+    parseHash (UrlParser.s "project" </> UrlParser.int) location
 
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    case (parseIdFromLocation location) of
+        Just value ->
+            { initialModel | current = value } ! []
+        Nothing ->
+            initialModel ! []
 
 
-toUrl : Model -> String
-toUrl model =
-    "#/" ++ toString (.current model)
-
-
-fromUrl : String -> Result String Int
-fromUrl url =
-    String.toInt (String.dropLeft 2 url)
-
-
-urlParser : Navigation.Parser (Result String Int)
-urlParser =
-    Navigation.makeParser (fromUrl << .hash)
-
-
-urlUpdate : Result String Int -> Model -> ( Model, Cmd Msg )
-urlUpdate result model =
-    case result of
-        Ok newIndex ->
-            ( { model | current = newIndex }, Cmd.none )
-
-        Err _ ->
-            ( initialModel, Cmd.none )
-
-
-main : Program Never
+main : Program Never Model Msg
 main =
-    Navigation.program urlParser
+    Navigation.program UrlChange
         { init = init
         , view = view
         , update = update
-        , urlUpdate = urlUpdate
-        , subscriptions = subscriptions
+        , subscriptions = (always Sub.none)
         }
